9ff276b2001c465876419f51432613466cb941e2
zhaojun <chunlaiqingke@gmail.com>
2025-07-11 02:24:44 +0800

    add

--- ddz_server/src/main/java/com/handsome/ddz/game/Carder.java
+++ ddz_server/src/main/java/com/handsome/ddz/game/Carder.java
@@ -3,6 +3,7 @@ package com.handsome.ddz.game;
 import lombok.Data;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Data
 public class Carder {
@@ -93,6 +94,148 @@ public class Carder {
 
         return result;
     }
+
+    public boolean isOneCard(List<Card> cardList) {
+        if (cardList == null || cardList.isEmpty()) {
+            return false;
+        }
+        return cardList.size() == 1;
+    }
+
+    //是否对子
+    public boolean isDoubleCard (List<Card> cardList){
+        if (cardList == null || cardList.isEmpty()) {
+            return false;
+        }
+        if(cardList.size() != 2){
+            return false;
+        }
+        //cardList[0].value==undefined说明是大小王，值是存储在king字段
+        return cardList.get(0).getValue() != null
+                && Objects.equals(cardList.get(1).getValue(), cardList.get(0).getValue());
+    }
+
+    //三张不带
+    public boolean isThreeCard (List<Card> cardList){
+        if(cardList.size() != 3){
+            return false;
+        }
+        //不能是大小王
+        boolean hasKing = cardList.stream().anyMatch(card -> card.getKing() != null);
+        if (hasKing) {
+            return false;
+        }
+        //判断三张牌是否相等
+        long count = cardList.stream().map(Card::getValue).distinct().count();
+        return count == 1;
+    }
+
+    //三带一
+    public boolean isThreeAndOne(List<Card> cardList) {
+        if(cardList.size() != 4){
+            return false;
+        }
+        List<Card> numCards = cardList.stream().filter(card -> card.getValue() != null).collect(Collectors.toList());
+        if (numCards.size() < 3) {
+            return false;
+        }
+        Map<Integer, List<Card>> cardMap = numCards.stream().collect(Collectors.groupingBy(Card::getValue));
+        for (Integer key : cardMap.keySet()) {
+            List<Card> value = cardMap.get(key);
+            if (value.size() == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isThreeAndTwo(List<Card> cardList) {
+        if(cardList.size() != 5){
+            return false;
+        }
+        List<Card> numCards = cardList.stream().filter(card -> card.getValue() != null).collect(Collectors.toList());
+        if (numCards.size() < 5) {
+            return false;
+        }
+        Map<Integer, List<Card>> cardMap = numCards.stream().collect(Collectors.groupingBy(Card::getValue));
+        if (cardMap.size() != 2) {
+            return false;
+        }
+        List<List<Card>> values = new ArrayList<>(cardMap.values());
+        List<Card> cards0 = values.get(0);
+        List<Card> cards1 = values.get(1);
+        if (cards0.size() == 3 && cards1.size() == 2 || cards0.size() == 2 && cards1.size() == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBoom(List<Card> cardList) {
+        if (cardList.size() != 4) {
+            return false;
+        }
+        long count = cardList.stream().map(Card::getValue).distinct().count();
+        return count == 1;
+    }
+
+    public boolean isKingBoom(List<Card> cardList) {
+        if (cardList.size() != 2) {
+            return false;
+        }
+        Integer king0 = cardList.get(0).getKing();
+        Integer king1 = cardList.get(1).getKing();
+        return king0 == 14 && king1 == 15 || king0 == 15 && king1 == 14;
+    }
+
+    public boolean isPlan(List<Card> cardList) {
+        if (cardList.size() != 6) {
+            return false;
+        }
+        List<Card> numCards = cardList.stream().filter(card -> card.getValue() != null).collect(Collectors.toList());
+        if (numCards.size() < 6) {
+            return false;
+        }
+        Map<Integer, List<Card>> cardMap = numCards.stream().collect(Collectors.groupingBy(Card::getValue));
+        if (cardMap.size() != 2) {
+            return false;
+        }
+        List<List<Card>> values = new ArrayList<>(cardMap.values());
+        if (values.get(0).size() != 3 || values.get(1).size() != 3) {
+            return false;
+        }
+        Integer value0 = values.get(0).get(0).getValue();
+        Integer value1 = values.get(1).get(0).getValue();
+        return Math.abs(value0 - value1) == 1
+                && (value0 != 12 && value1 != 12);
+    }
+
+    //飞机带2单
+    public boolean isPlanWithSingle(List<Card> cardList) {
+        if (cardList.size() != 8) {
+            return false;
+        }
+        List<Card> numCards = cardList.stream().filter(card -> card.getValue() != null).collect(Collectors.toList());
+        if (numCards.size() < 6) {
+            return false;
+        }
+        Map<Integer, List<Card>> valueMap = numCards.stream().collect(Collectors.groupingBy(Card::getValue));
+
+        int[] nums = new int[2];
+        int threeCount = 0;
+        for (Integer value : valueMap.keySet()) {
+            List<Card> cards = valueMap.get(value);
+            if (cards.size() == 3) {
+                nums[0] = value;
+                threeCount ++;
+            }
+        }
+        if (threeCount != 2) {
+            return false;
+        }
+        List<Card> collect = numCards.stream().filter(card -> card.getValue() == nums[0] || card.getValue() == nums[1]).collect(Collectors.toList());
+        return this.isPlan(collect);
+    }
+
 
     public Object isCanPushs(Integer data) {
         return null;
